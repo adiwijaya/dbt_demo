fb84156df15d69f9bca08cc5bf84610fed4f2b24
adiwijaya <adiwijaya25@gmail.com>
2022-01-21 08:03:38 +0000

    dev

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export PROJECT_ID='pso-dbt-airflow-demo'
-export USER_NAME='admin_adiwijaya_me'
 
 dbt run  --vars '{"project_id": '$PROJECT_ID',"user_name": '$USER_NAME'}' --profiles-dir .dbt 
 dbt test  --vars '{"project_id": '$PROJECT_ID',"user_name": '$USER_NAME'}' --profiles-dir .dbt 
